53fa75c9514f963e4ace864b8957e1d0c0f70087
Martinaroque <martirocklx@hotmail.com>
2023-07-20 12:24:00 +0100

    update the correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      iconUrl: `https://ssl.gstatic.com/onebox/weather/64/sunny.png`,
+      iconUrl: ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
     });
@@ -60,6 +60,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
+    search();
     return "Loading..";
   }
 }
